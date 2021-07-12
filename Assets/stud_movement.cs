c4193eaa94f06ef9b01652d8d61ea4cf2052f6c2
IluvSamsung <bys100100@gmail.com>
2021-07-13 01:57:53 +0900

    ㅈㄱ
    ㅈㄱ

--- Assets/stud_movement.cs
+++ Assets/stud_movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class stud_movement : MonoBehaviour
 {
-    const float speed = 20;
+    const float speed = 5;
     public GameObject mainChar;
     bool isRunning = false;
     Animator ani;
